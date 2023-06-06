07a1e2eb801e0043675587c1d7e4f822e9efeccd
Rahul Shaw <rahul.shaw505@gmail.com>
2023-06-06 21:29:06 +0530

    added dashboard cards

--- src/app/Core/dashboard/dashboard.component.ts
+++ src/app/Core/dashboard/dashboard.component.ts
@@ -3,8 +3,80 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-
+  cardArray = [
+    {
+      _id: 0,
+      groupIcon: 'fa fa-sitemap',
+      groupName: 'Site Statistics',
+      card: [
+        {
+          cardIcon: '../../../assets/dashboard icon/person-circle.svg',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Total Riders',
+          cardContentSubheading: '329',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/driver-dash.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Total Drivers',
+          cardContentSubheading: '500',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/car.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Vehicle Type',
+          cardContentSubheading: '4',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/money.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Total Drivers',
+          cardContentSubheading: '500',
+          cardColor: 'green',
+        },
+      ],
+    },
+    {
+      _id: 1,
+      groupIcon: 'fa fa-list-alt',
+      groupName: 'Ride Statistics',
+      card: [
+        {
+          cardIcon: '../../../assets/dashboard icon/driver-dash.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Total Ride',
+          cardContentSubheading: '821',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/car.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Cancel Ride',
+          cardContentSubheading: '104',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/money.png',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Running Ride',
+          cardContentSubheading: '500',
+          cardColor: 'green',
+        },
+        {
+          cardIcon: '../../../assets/dashboard icon/person-circle.svg',
+          cardIconBGcolor: '#49978b',
+          cardContentHeading: 'Completed',
+          cardContentSubheading: '329',
+          cardColor: 'green',
+        },
+      ],
+    },
+  ];
+  
 }
