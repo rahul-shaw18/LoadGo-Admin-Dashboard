82b3b6a1caccb457d77dd054f624f24a902be7bc
Rahul Shaw <rahul.shaw505@gmail.com>
2023-06-10 13:05:07 +0530

    added rider component

--- src/app/Core/riders/riders.component.ts
+++ src/app/Core/riders/riders.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { TableService } from 'src/app/Shared/services/table.service';
 
 @Component({
   selector: 'app-riders',
   templateUrl: './riders.component.html',
-  styleUrls: ['./riders.component.scss']
+  styleUrls: ['./riders.component.scss'],
 })
 export class RidersComponent {
+  tableData: any;
 
+  constructor(private tableService: TableService) {}
+
+  ngOnInit() {
+    this.tableData = this.tableService.getTableData();
+  }
+
+  // table
+  displayedColumns: string[] = [
+    'rideId',
+    'riderName',
+    'driverName',
+    'pickDropAddress',
+    'date',
+    'rideFare',
+    'status',
+  ];
 }
