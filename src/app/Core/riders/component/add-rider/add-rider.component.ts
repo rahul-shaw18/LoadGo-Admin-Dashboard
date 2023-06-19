dcb0d902e28fc9f3363d8355c2679ee9511bd96f
Rahul Shaw <rahul.shaw505@gmail.com>
2023-06-19 22:54:38 +0530

    added add-rider content

--- src/app/Core/riders/component/add-rider/add-rider.component.ts
+++ src/app/Core/riders/component/add-rider/add-rider.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -7,8 +8,63 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./add-rider.component.scss'],
 })
 export class AddRiderComponent {
+  myForm!: FormGroup;
+  profileImage: any;
   constructor(
     public dialogRef: MatDialogRef<AddRiderComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder
   ) {}
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      contact: ['', Validators.required],
+      gender: ['', Validators.required],
+      profileImage: [null, Validators.required],
+    });
+  }
+
+  get gf() {
+    return this.myForm.controls;
+  }
+
+  onSubmit() {
+    // Handle form submission logic here
+    // Access form values using this.myForm.value
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.readFile(file);
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDrop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    const file = event.dataTransfer.files[0];
+    this.readFile(file);
+  }
+
+  private readFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.profileImage = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onReset() {}
 }
