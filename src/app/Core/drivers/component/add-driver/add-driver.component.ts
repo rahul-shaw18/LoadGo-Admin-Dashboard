56e96d08ef05eaa6f88f970b7a4592599c2a4003
Rahul Shaw <rahul.shaw505@gmail.com>
2023-06-20 22:06:20 +0530

    added content in add-driver component

--- src/app/Core/drivers/component/add-driver/add-driver.component.ts
+++ src/app/Core/drivers/component/add-driver/add-driver.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -7,8 +8,63 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./add-driver.component.scss'],
 })
 export class AddDriverComponent {
+  myForm!: FormGroup;
+  profileImage: any;
+  formBuilder: any;
   constructor(
     public dialogRef: MatDialogRef<AddDriverComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      driverName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      contact: ['', Validators.required],
+      vehicleType: ['', Validators.required],
+      profileImage: [null, Validators.required],
+    });
+  }
+
+  get gf() {
+    return this.myForm.controls;
+  }
+
+  onSubmit() {
+    // Handle form submission logic here
+    // Access form values using this.myForm.value
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.readFile(file);
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDrop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    const file = event.dataTransfer.files[0];
+    this.readFile(file);
+  }
+
+  private readFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.profileImage = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onReset() {}
 }
